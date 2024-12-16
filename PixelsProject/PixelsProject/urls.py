f6c37398dc1b879ce8a8f711e484db35e6de4db2
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 12:19:14 +0200

    update pixel app path

--- PixelsProject/PixelsProject/urls.py
+++ PixelsProject/PixelsProject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("pixels", include("pixels.urls")),
+    path(" ", include("pixels.urls")),
 ]
