bf1d21bc6220117eb90ed825e0e00a8c03b8b971
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 20:29:13 +0200

    update app urls to route views

--- PixelsProject/pixels/urls.py
+++ PixelsProject/pixels/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .import views
 
 app_name = 'pixels'
 
 urlpatterns = [
-    
+    path('', views.image_list, name='image_list'),
+    path('upload/', views.upload_image, name='upload_image'),
+    path('image/<int:pk>/', views.image_detail, name='image_details'),
 ]
