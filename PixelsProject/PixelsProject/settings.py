d4edd55a244f3831602e63e26a073b476a12312a
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 12:16:02 +0200

    config Media handling in settings (user-uploaded content)

--- PixelsProject/PixelsProject/settings.py
+++ PixelsProject/PixelsProject/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] #app static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #collection of static files to be (served with Nginx() in production)
 
+#Media files (User-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
