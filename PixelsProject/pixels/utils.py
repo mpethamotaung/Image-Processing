0186847f9c1f7614cb34ba0d53726579c57ce45d
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 18:28:59 +0200

    update utils add BytesIO to process images without writing them to disk unnecessarily.

--- PixelsProject/pixels/utils.py
+++ PixelsProject/pixels/utils.py
@@ -1,17 +1,32 @@
 from PIL import Image
+from io import BytesIO
 
-def get_center_hex(image_path):
-    with Image.open(image_path) as img:
-        img = img.convert("RBG")
-        width, height = img.size
-        center_pixels = [
-            img.getpixel((width//2, height//2)),
-            img.getpixel((width//2-1, height//2)),
-            img.getpixel((width//2, height//2-1)),
-            img.getpixel((width//2-1, height//2-1)),
-        ]
-        avg_color = tuple(
-            sum(pixel[i] for pixel in center_pixels)//len(center_pixels)
-            for i in range(3)
-        )
-        return "#{:02x}{:02x}{:02x}".format(*avg_color)
+def get_center_hex(image_file):
+    """
+    Reads an image file and calculates the average
+    color of its center pixels.
+    """
+    try:
+        with Image.open(BytesIO(image_file.read())) as img:
+            img = img.convert("RGB")
+            width,height = img.size
+
+            #Extract center-most pixels
+            center_pixels = [
+                img.getpixel((width // 2, height // 2)),
+                img.getpixel((width // 2 - 1, height // 2)),
+                img.getpixel((width // 2, height // 2 - 1)),
+                img.getpixel((width // 2 - 1, height // 2 - 1)),
+            ]
+
+            #Calculate the average color
+            avg_color = tuple(
+                sum(pixel[i] for pixel in center_pixels) // len(center_pixels)
+                for i in range(4)
+            )
+
+            #Convert to hex
+            return "#{:02x}{:02x}{:02x}".format(*avg_color)
+    except Exception as e:
+        raise ValueError(f"Failed to process image")
+        
