f7959bc3b5c634e18b686af00f1c9874b003575e
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 14:04:37 +0200

    update static url and media root directory

--- PixelsProject/PixelsProject/settings.py
+++ PixelsProject/PixelsProject/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] #app static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #collection of static files to be (served with Nginx() in production)
 
 #Media files (User-uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
