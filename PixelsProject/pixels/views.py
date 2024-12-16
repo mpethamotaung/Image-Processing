1f26d554e5bdc16ff46a06e4ee5e131291bb16c9
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 19:44:56 +0200

    create views to process uploaded images, import forms, import models, import utils

--- PixelsProject/pixels/views.py
+++ PixelsProject/pixels/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ImageUploadForm
+from .models import ImageColor
+from .utils import get_center_hex
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image_instance = form.save(commit=False)
+
+            #Pillow + BytesIO to get the hex color
+            try:
+                image_instance.hex_color = get_center_hex(image_instance.image)
+                image_instance.save()
+                return redirect('image_list')
+            except ValueError as e:
+                form.add_error('image', f"Image processing failed: {str(e)}")
+        else:
+            form = ImageUploadForm()
+        return render(request, 'pixels/upload.html', {'form':form})
+
+def image_list(request):
+    images = ImageColor.objects.all()
+    return render(request, 'pixels/image_list.html', {'image': images})
+
+def image_detail(request, pk):
+    image = ImageColor.objects.get(pk=pk)
+    return render(request, 'pixels/image_details.html', {'image': image})
+
 
 
