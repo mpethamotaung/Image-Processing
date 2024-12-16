50f8829a9aac0e07791f0d12a052a9d33285900c
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 13:06:34 +0200

    create initial models.py file for pixels app, might modify later

--- PixelsProject/pixels/models.py
+++ PixelsProject/pixels/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ImageColor(models.Model):
+
+    image = models.ImageField(upload_to='uploads/')
+    hex_color = models.CharField(max_length=7)
+
+    def __str__(self):
+        return f"{self.image.name} - {self.hex_color}"
