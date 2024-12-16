82d16491e8fcecdc248112dc76083fa7fec85b79
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-16 22:46:11 +0200

    update main urls.py file to fix root URL error

--- PixelsProject/PixelsProject/urls.py
+++ PixelsProject/PixelsProject/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(" ", include("pixels.urls")), #Pixel app urls
+    path('', include("pixels.urls")), #Pixel app urls
 ]
 
 if settings.DEBUG:
