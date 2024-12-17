5f8044cb4f03f8d697cc74852de35f8198690458
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-17 18:31:10 +0200

    update image_detail, fix typo so that Django can find the file

--- PixelsProject/pixels/urls.py
+++ PixelsProject/pixels/urls.py
@@ -5,6 +5,6 @@ app_name = 'pixels'
 
 urlpatterns = [
     path('', views.image_list, name='image_list'), #Roor path
-    path('upload/', views.upload_image, name='upload_image'),
+    path('upload/', views.upload_image, name='upload'),
     path('image/<int:pk>/', views.image_detail, name='image_details'),
 ]
