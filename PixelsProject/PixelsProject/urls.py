76d752ec55472225a72d49084a385771f561d690
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-18 00:13:17 +0200

    include urlpattern for drf path [path('api-auth/', include('rest_framework.urls')),]

--- PixelsProject/PixelsProject/urls.py
+++ PixelsProject/PixelsProject/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('pixels.urls', namespace='pixels')), #Pixel app urls
+    path('api-auth/', include('rest_framework.urls')), #needed for browsable API
 ]
 
 if settings.DEBUG:
