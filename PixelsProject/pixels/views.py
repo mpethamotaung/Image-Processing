0c5df6093fae592918fbd82d678e5bbfc0c44214
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-17 11:14:34 +0200

    update views to return cleaned data and call util function in utils.py to extract hex value

--- PixelsProject/pixels/views.py
+++ PixelsProject/pixels/views.py
@@ -3,22 +3,33 @@ from .forms import ImageUploadForm
 from .models import ImageColor
 from .utils import get_center_hex
 
+
 def upload_image(request):
+    """
+    Handles image uploads and process the center-most pixel color
+    """
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            image_instance = form.save(commit=False)
 
-            #Pillow + BytesIO to get the hex color
+        if form.is_valid():
+
+            image = form.cleaned_data['image']
+
             try:
-                image_instance.hex_color = get_center_hex(image_instance.image)
+                #Call the utility function to get center hex color
+                hex_color = get_center_hex(image)
+                #save the image and its hex value in db
+                image_instance = ImageColor.objects.create(image=image, hex_color=hex_color)
                 image_instance.save()
+                #Redicrect to image list page
                 return redirect('image_list')
             except ValueError as e:
-                form.add_error('image', f"Image processing failed: {str(e)}")
-        else:
-            form = ImageUploadForm()
-        return render(request, 'pixels/upload.html', {'form':form})
+                #This to inform the user if processing fails
+                form.add_error(f'Failed to upload image', {'image': image})
+    else:
+        form = ImageUploadForm()
+    #Render the uploaded image with the form
+    return render(request, 'pixels/upload.html', {'form': form})
 
 def image_list(request):
     images = ImageColor.objects.all()
