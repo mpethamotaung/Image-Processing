53121b92ac004401d9cf769b9e458ad69a749bc1
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-18 14:56:34 +0200

    update serializers

--- PixelsProject/pixels/serializers.py
+++ PixelsProject/pixels/serializers.py
@@ -4,4 +4,11 @@ from .models import ImageColor
 class UploadedImageSerializers(serializers.ModelSerializer):
     class Meta:
         model = ImageColor
-        fields = ['pk', 'image', 'hex_color']
+        fields = ['pk', 'image', 'hex_color'] 
+        
+        # We can also use fields = '__all__' to obtain all model fields
+        """
+        Used pk(primary key) instead in case we want to change the primary key
+        to another field (e.g, uuid, slug). pk will still refer to the primary key
+        but if we use id, we would have to rewrite the code.
+        """
