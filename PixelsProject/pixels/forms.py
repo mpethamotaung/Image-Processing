965350c8e4e51b5cf28a4544c8831bf3e2b808e2
Hanma-yujiro <mpethamotaung@gmail.com>
2024-12-20 11:04:42 +0200

    update forms to handle large images max file size

--- PixelsProject/pixels/forms.py
+++ PixelsProject/pixels/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 from .models import ImageColor
+from django.core.exceptions import ValidationError
 
 class ImageUploadForm(forms.ModelForm):
+
+    MAX_FILE_SIZE = 5 * 1024 * 1024
+    
     class Meta:
         model = ImageColor
         fields = ['image']
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+
+        #Check for file size
+        if image.size > self.MAX_FILE_SIZE:
+            raise ValidationError(f"The file is too large. The maximum file size is {self.MAX_FILE_SIZE/(1920*1080)} MB.")
+
+        return image
